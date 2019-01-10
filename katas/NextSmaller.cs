ce58bef0950c972e89ac6cf8977e30b8500376ce
Michael Wirth <mail@michaelwirth.de>
2019-01-10 22:32:17 +0100

    beschleunigt

--- katas/NextSmaller.cs
+++ katas/NextSmaller.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -10,68 +11,83 @@ namespace katas
     {
         public static long NextSmaller(long n)
         {
-            var originalDigits = GetDigits(n);
-            var lists = originalDigits.Permute();
-            var permutations = new Dictionary<string, long>();
-            foreach (var item in lists)
-            {
-                var numstring = String.Join("", item);
-                var number = long.Parse(numstring);
-                if (!permutations.ContainsKey(numstring))
-                    permutations.Add(numstring, number);
-            }
-
-            if (!permutations.Any())
-                return -1;
-
-            var sorted = permutations.OrderByDescending(p => p.Value);
-            var result = sorted.FirstOrDefault(num => num.Value < n);
-
-            if (result.Key == null)
-                return -1;
-
-            if (result.Key.StartsWith("0"))
-                return -1;
-            return result.Value;
-        }
-        private static List<long> GetDigits(long n)
-        {
-            var digits = new List<long>();
-            do
-            {
-                digits.Add(n % 10);
-                n = n / 10;
-            } while (n > 0);
-            return digits;
+            var result = n.ToString().NextSmaller(n.ToString().Length);
+            return result;
         }
     }
 
-    public static class ListExtensions
+    public static class longExtensions
     {
-        public static IEnumerable<List<T>> Permute<T>(this IList<T> items)
+        public static long NextSmaller(this string number, int lenght)
         {
-            var indices = Enumerable.Range(0, items.Count).ToArray();
-            yield return indices.Select(idx => items[idx]).ToList();
-            var weights = new int[items.Count];
-            var idxUpper = 1;
-            while (idxUpper < items.Count)
+            int i, j;
+
+            // I) Start from the right most digit 
+            // and find the first digit that is 
+            // smaller than the digit next to it. 
+            for (i = lenght - 1; i > 0; i--)
+                if (number[i] < number[i - 1])
+                    break;
+
+            // If no such digit is found 
+            // then all digits are in ascending order 
+            // means there cannot be a smallest number 
+            // with same set of digits 
+            if (i == 0)
             {
-                if (weights[idxUpper] < idxUpper)
-                {
-                    var idxLower = idxUpper % 2 * weights[idxUpper];
-                    var tmp = indices[idxLower];
-                    indices[idxLower] = indices[idxUpper];
-                    indices[idxUpper] = tmp;
-                    yield return indices.Select(idx => items[idx]).ToList();
-                    weights[idxUpper]++;
-                    idxUpper = 1;
-                }
-                else
-                {
-                    weights[idxUpper] = 0;
-                    idxUpper++;
-                }
+                return -1;
             }
+
+            // II) Find the greatest digit on 
+            // right side of (i-1)'th digit that is 
+            // smaller than number[i-1] 
+            int x = number[i - 1], greatest = i;
+            for (j = i; j < lenght; j++)
+                if (number[j] < x && number[j] > number[greatest])
+                    greatest = j;
+
+
+            // III) Swap the above found smallest digit with number[i-1] 
+            var result = SwapCharacters(number, greatest, i - 1);
+
+            // IV) Sort the digits after (i-1) in descending order 
+            var sortedResult = Sort(result, i);
+
+            if (sortedResult.StartsWith("0"))
+                return -1;
+
+            return long.Parse(sortedResult);
+        }
+
+        private static string Sort(string number, int index)
+        {
+            char[] array = number.ToArray();
+            char[] part = new char[array.Length - index];
+            char[] front = new char[index];
+            Array.Copy(array, 0, front, 0, index);
+            Array.Copy(array, index, part, 0, array.Length - index);
+            var sorted = part.OrderByDescending(c => c).ToArray();
+            var result = $"{new string(front)}{new string(sorted)}";
+            return result;
+        }
+
+        private static string SwapCharacters(string value, int position1, int position2)
+        {
+            char[] array = value.ToCharArray();
+            char temp = array[position1];
+            array[position1] = array[position2];
+            array[position2] = temp;
+            return new string(array);
         }
     }
+    public class CompareDescending : IComparer<char>
+    {
+        public int Compare(char x, char y)
+        {
+            if (x > y) return 0;
+            else return -1;
+        }
+    }
+
+
 }
